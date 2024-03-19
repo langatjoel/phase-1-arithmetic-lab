e98b4799dc649c6c6594ba8e5e35ede269398b52
joel langat <langatjoel42@gmail.com>
2024-03-19 13:00:15 +0300

    made some variables in index.js

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10);
+const num3 = 20;
+const num4 = 8;
+const mod = num3 / num4;
+const max = Math.max(1, 2, 20, 5);
